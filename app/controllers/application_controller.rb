abded496f49d3e8cd708efa9edcd860a69be9442
sugiwa <sugi.warra@gmail.com>
2021-04-11 15:57:39 +0900

    change strong parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,10 +15,6 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    added_attrs = [:name, :email, :password, :password_confirmation, :remember_me]
-    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
-    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+    devise_parameter_sanitizer.permit :sign_up, keys: [:email]
   end
-  
-  
 end
